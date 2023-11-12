1850aac7a00c5979871dae332a684e7540c65c49
bahromoken <magdiyevbahrom@gmail.com>
2023-11-12 13:05:03 +0500

    modify cources and library models.py

--- courses/admin.py
+++ courses/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import MainSubClass, SubClass, Comment, Course
 
-
+# task:change new models name
 admin.site.register(MainSubClass)
 admin.site.register(SubClass)
 admin.site.register(Comment)
