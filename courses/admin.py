9a0aeb00627a80caaa3e089d03db96ddb833f2c9
bahromoken <magdiyevbahrom@gmail.com>
2023-12-14 18:58:45 +0500

    fix app imports

--- courses/admin.py
+++ courses/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from courses.models import Course, Chapter, Lesson, UserLessonRewiew, CourseComment
 
+
 admin.site.register(Course)
 admin.site.register(Chapter)
 admin.site.register(Lesson)
